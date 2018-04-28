ff1ff8195b9ae635977b8de81d3bfa30c46e5ad3
frederic <fredericmbea@gmail.com>
2018-04-28 15:13:04 -0400

    edit the Homecontroller spec create 2 contexts the first context is when the user is not signed in the second context is when the user is signed in When the user is not signed in, the response object should not render the index template

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,16 +5,28 @@ RSpec.describe HomeController, type: :controller do
                                        email: "tester@testdomain.test",
                                        password: "password") }
 
-  before do
-    request.env['devise.mapping'] = Devise.mappings[:user]
-    sign_in user
-  end
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(200)
+    context "when the user is not signed in" do
+      it "redirect_to the login page" do
+        get :index
+        expect(response).not_to render_template(:index)
+      end
     end
+
+    context "when the user is signed in" do
+
+      before do
+        request.env['devise.mapping'] = Devise.mappings[:user]
+        sign_in user
+      end
+
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+    end
+
   end
 
 end
