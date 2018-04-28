e3cd8e0355754d37efcc121f9bffd2820bd90038
frederic <fredericmbea@gmail.com>
2018-04-28 11:48:25 -0400

    Describe book inventory listing

---
+++ features/step_definitions/book_inventory_steps.rb
@@ -0,0 +1,7 @@
+Given("I have populated my inventory with several books") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the list of my books") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
